9f7ff3b09201a54379ff7a09926f566886ac8cf6
OksanaZhoha <zhoga.oksana@gmail.com>
2024-02-24 12:25:01 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,34 +1,14 @@
-import axios from 'axios';
+export const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '42494915-f1925227b1d4f602df08c38a4';
 
-export class PixabayAPI {
-  #API_KEY = '42494915-f1925227b1d4f602df08c38a4';
-  #BASE_URL = 'https://pixabay.com/api/';
-
-  baseSearchParams = {
-    key: this.#API_KEY,
+export const options = {
+  params: {
+    key: API_KEY,
+    q: '',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: 1,
     per_page: 40,
-  };
-
-  constructor() {
-    this.query = '';
-    this.page = 1;
-  }
-
-  async fetchHits() {
-    const searchParams = new URLSearchParams({
-      ...this.baseSearchParams,
-      q: this.query,
-      page: this.page,
-    });
-
-    const response = await axios.get(`${this.#BASE_URL}?${searchParams}`);
-    return response;
-  }
-
-  restPage() {
-    this.page = 1;
-  }
-}
+  },
+};
