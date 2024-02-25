a7acde4d5542d1f1472a86bf6a56ed51e653f40c
OksanaZhoha <zhoga.oksana@gmail.com>
2024-02-25 11:36:10 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,22 +1,15 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { BASE_URL, options } from './pixabay-api.js';
-
-const galleryEl = document.querySelector('.gallery');
-const searchInputEl = document.querySelector('input[name="searchQuery"');
-const searchFormEl = document.getElementById('search-form');
+import { options } from './pixabay-api.js';
 
 const lightbox = new SimpleLightbox('.lightbox', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-let totalHits = 0;
-let reachedEnd = false;
-
-function renderGallery(hits) {
+export function renderGallery(hits, totalHits) {
+  const galleryEl = document.querySelector('.gallery');
   const markup = hits
     .map(
       ({
@@ -60,64 +53,7 @@ function renderGallery(hits) {
   galleryEl.insertAdjacentHTML('beforeend', markup);
 
   if (options.params.page * options.params.per_page >= totalHits) {
-    if (!reachedEnd) {
-      Notify.info("We're sorry, but you've reached the end of search results.");
-      reachedEnd = true;
-    }
+    Notify.info("We're sorry, but you've reached the end of search results.");
   }
   lightbox.refresh();
 }
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  options.params.q = searchInputEl.value.trim();
-  if (options.params.q === '') {
-    return;
-  }
-  options.params.page = 1;
-  galleryEl.innerHTML = '';
-  reachedEnd = false;
-
-  try {
-    const response = await axios.get(BASE_URL, options);
-    totalHits = response.data.totalHits;
-
-    const { hits } = response.data;
-    console.log(hits);
-
-    if (hits.length === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      Notify.success(`Hooray! We found ${totalHits} images.`);
-      renderGallery(hits);
-    }
-    searchInputEl.value = '';
-  } catch (err) {
-    Notify.failure(err);
-  }
-}
-
-searchFormEl.addEventListener('submit', handleSubmit);
-
-async function loadMore() {
-  options.params.page += 1;
-  try {
-    const response = await axios.get(BASE_URL, options);
-    const hits = response.data.hits;
-    renderGallery(hits);
-  } catch (err) {
-    Notify.failure(err);
-  }
-}
-
-function handleScroll() {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-  if (scrollTop + clientHeight >= scrollHeight) {
-    loadMore();
-  }
-}
-
-window.addEventListener('scroll', handleScroll);
